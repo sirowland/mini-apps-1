63ae7c4898e288aeabe0642454cb27e5b9149f7c
Sam Rowland <srowland2594@gmail.com>
2018-05-21 13:30:26 -0700

    Working solution of basic reqs achieved.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,5 +1,10 @@
 var playerXTurn = true;
-var boardPieces = ["1-1","1-2","1-3","2-1","2-2","2-3","3-1","3-2","3-3"];
+var pieceCount = 0;
+var boardPieces = [["1-1","1-2","1-3"],["2-1","2-2","2-3"],["3-1","3-2","3-3"]];
+var boardPiecesCol = [["1-1","2-1","3-1"],["1-2","2-2","3-2"],["1-3","2-3","3-3"]];
+var majDiag = [["1-1","2-2","3-3"]];
+var minDiag = [["1-3","2-2","3-1"]];
+var board = boardPieces.concat(boardPiecesCol).concat(majDiag).concat(minDiag);
 
 function placePiece(domElement) {
     var id = domElement.id;
@@ -19,20 +24,60 @@ function placePiece(domElement) {
     }
 }
 
+function checkForWin() {
+
+    function check() {
+        //checks horizontal direction;
+        for (var i = 0; i < board.length; i++) {
+            var xWin = board[i].every((piece) => document.getElementById(piece).innerHTML === "[x]");
+            var oWin = board[i].every((piece) => document.getElementById(piece).innerHTML === "[o]");
+            
+            if (xWin) {
+                return "x"
+            } else if (oWin) {
+                return "o"
+            }
+        }
+    }
+
+    var win = check();
+    
+    if (win === "x") {
+        return "X Wins!";
+    } else if (win === "o") {
+        return "O Wins!";
+    } else if (pieceCount === 9) {
+        return "Tied!"
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function() {
 
     //initialization add event listeners
     for (var i = 0; i < boardPieces.length; i++) {
-        document.getElementById(boardPieces[i]).addEventListener("click", function() {
-            placePiece(this);
-        })
+        for (var j = 0; j < boardPieces[i].length; j++) {
+            document.getElementById(boardPieces[i][j]).addEventListener("click", function() {
+                placePiece(this);
+                pieceCount++;
+
+                var output = checkForWin();
+
+                if (output) {
+                    document.getElementById("result").innerHTML = output;
+                }
+            })
+        }
     }
 
     //0 out all of the pieces and reset turns
     document.getElementById("reset").addEventListener("click", function() {
         for (var i = 0; i < boardPieces.length; i++) {
-            document.getElementById(boardPieces[i]).innerHTML = "[]";
+            for (var j = 0; j < boardPieces[i].length; j++) {
+                document.getElementById(boardPieces[i][j]).innerHTML = "[]";
+            }
         }
         playerXTurn = true;
+        pieceCount = 0;
+        document.getElementById("result").innerHTML = '';
     })
 });
