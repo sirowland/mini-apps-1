85b6b0e53addfd1ac98c6624fd7c7d85acd7d033
Sam Rowland <srowland2594@gmail.com>
2018-05-21 17:46:23 -0700

    Started refactor, works again but still more to do.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,122 +1,139 @@
-//init vals
-var playerXTurn = true;
-var pieceCount = 0;
-var xScore = 0;
-var oScore = 0;
-var lastWin = null;
-var p1Name = null;
-var p2Name = null;
+var board = {
+    boardPieces: [["1-1","1-2","1-3"],["2-1","2-2","2-3"],["3-1","3-2","3-3"]],
+    boardPiecesCol: [["1-1","2-1","3-1"],["1-2","2-2","3-2"],["1-3","2-3","3-3"]],
+    majDiag: [["1-1","2-2","3-3"]],
+    minDiag: [["1-3","2-2","3-1"]],
+    playerXTurn: true,
+    pieceCount: 0,
+    xScore: 0,
+    oScore: 0,
+    lastWin: null,
 
-//Create board
-var boardPieces = [["1-1","1-2","1-3"],["2-1","2-2","2-3"],["3-1","3-2","3-3"]];
-var boardPiecesCol = [["1-1","2-1","3-1"],["1-2","2-2","3-2"],["1-3","2-3","3-3"]];
-var majDiag = [["1-1","2-2","3-3"]];
-var minDiag = [["1-3","2-2","3-1"]];
-var board = boardPieces.concat(boardPiecesCol).concat(majDiag).concat(minDiag);
-
-function placePiece(domElement) {
-    var id = domElement.id;
-
-    //check if piece already placed
-    if (document.getElementById(id).innerHTML === "X" || document.getElementById(id).innerHTML === "O") {
-        return;
-    }
-
-    //place piece and flip turn
-    if (playerXTurn === true) {
-        document.getElementById(id).innerHTML = "X";
-        playerXTurn = !playerXTurn;
-    } else {
-        document.getElementById(id).innerHTML = "O"
-        playerXTurn = !playerXTurn;        
-    }
-}
-
-function checkForWin() {
-
-    function check() {
-        for (var i = 0; i < board.length; i++) {
-            var xWin = board[i].every((piece) => document.getElementById(piece).innerHTML === "X");
-            var oWin = board[i].every((piece) => document.getElementById(piece).innerHTML === "O");
-            
-            if (xWin) {
-                return "x"
-            } else if (oWin) {
-                return "o"
-            }
-        }
-    }
-
-    var win = check();
+    placePiece: function(domElement) {
+        var id = domElement.id;
     
-    if (win === "x") {
-        xScore++;
-        document.getElementById("xscore").innerHTML =  p1Name ? p1Name + " (X) Score: " + xScore : " (X) Score: " + xScore;
-        return "X Wins!";
-    } else if (win === "o") {
-        oScore++;
-        document.getElementById("oscore").innerHTML =  p2Name ? p2Name + " (O) Score: " + oScore : " (O) Score: " + oScore;
-        return "O Wins!";
-    } else if (pieceCount === 9) {
-        return "Tied!"
+        //check if piece already placed
+        if (view.getInner(id) === "X" || view.getInner(id) === "O") {
+            return;
+        }
+    
+        //place piece and flip turn
+        if (this.playerXTurn === true) {
+            view.setInner(id, "X");
+            this.playerXTurn = !this.playerXTurn;
+        } else {
+            view.setInner(id, "O");
+            this.playerXTurn = !this.playerXTurn;        
+        }
+    },
+
+    checkForWin: function() {
+
+        var board = this.boardPieces.concat(this.boardPiecesCol).concat(this.majDiag).concat(this.minDiag);
+
+        function check() {
+            for (var i = 0; i < board.length; i++) {
+                var xWin = board[i].every((piece) => view.getInner(piece) === "X");
+                var oWin = board[i].every((piece) => view.getInner(piece) === "O");
+                
+                if (xWin) {
+                    return "x"
+                } else if (oWin) {
+                    return "o"
+                }
+            }
+        }
+    
+        var win = check();
+        
+        if (win === "x") {
+            this.xScore++;
+            var winString = view.p1Name ? view.p1Name + " (X) Score: " + this.xScore : " (X) Score: " + this.xScore;
+            view.setInner("xscore", winString);
+            return "X Wins!";
+        } else if (win === "o") {
+            this.oScore++;
+            var winString =  view.p2Name ? view.p2Name + " (O) Score: " + this.oScore : " (O) Score: " + this.oScore;
+            view.setInner("oscore", winString);
+            return "O Wins!";
+        } else if (this.pieceCount === 9) {
+            return "Tied!"
+        }
     }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    //initialize scores
-    document.getElementById("xscore").innerHTML = "(X) Score: " + xScore;
-    document.getElementById("oscore").innerHTML = "(O) Score: " + oScore;
+var view = {
+    p1Name: null,
+    p2Name: null,
 
-    //Deal with player 1 and player 2 Submissions
-    document.getElementById("p1").addEventListener("keyup", function(e){
-        if (e.keyCode === 13){
-            document.getElementById("xscore").innerHTML = e.target.value + " " + document.getElementById("xscore").innerHTML;
-            p1Name = e.target.value; 
-            document.getElementById("p1").remove();
-        }
-    });
+    getInner: function(id) {
+        return document.getElementById(id).innerHTML;
+    },
 
-    document.getElementById("p2").addEventListener("keyup", function(e){
-        if (e.keyCode === 13){
-            document.getElementById("oscore").innerHTML = e.target.value + " " + document.getElementById("oscore").innerHTML; 
-            p2Name = e.target.value;
-            document.getElementById("p2").remove();
-        }
-    });
+    setInner: function(id, value) {
+        document.getElementById(id).innerHTML = value;
+    },
 
-    //initialization add event listeners
-    for (var i = 0; i < boardPieces.length; i++) {
-        for (var j = 0; j < boardPieces[i].length; j++) {
-            document.getElementById(boardPieces[i][j]).addEventListener("click", function() {
-                placePiece(this);
-                pieceCount++;
-
-                var output = checkForWin();
-
-                if (output) {
-                    lastWin = output[0];
-                    document.getElementById("result").innerHTML = output;
+    init: function() {
+        document.addEventListener("DOMContentLoaded", function() {
+            //initialize scores
+            view.setInner("xscore", "(X) Score: " + board.xScore);
+            view.setInner("oscore", "(O) Score: " + board.oScore);
+        
+            //Deal with player 1 and player 2 Submissions
+            document.getElementById("p1").addEventListener("keyup", function(e){
+                if (e.keyCode === 13){
+                    document.getElementById("xscore").innerHTML = e.target.value + " " + document.getElementById("xscore").innerHTML;
+                    view.p1Name = e.target.value; 
+                    document.getElementById("p1").remove();
                 }
-            })
-        }
-    }
-
-    //0 out all of the pieces and reset turns
-    document.getElementById("reset").addEventListener("click", function() {
-        for (var i = 0; i < boardPieces.length; i++) {
-            for (var j = 0; j < boardPieces[i].length; j++) {
-                document.getElementById(boardPieces[i][j]).innerHTML = "";
+            });
+        
+            document.getElementById("p2").addEventListener("keyup", function(e){
+                if (e.keyCode === 13){
+                    document.getElementById("oscore").innerHTML = e.target.value + " " + document.getElementById("oscore").innerHTML; 
+                    view.p2Name = e.target.value;
+                    document.getElementById("p2").remove();
+                }
+            });
+        
+            //initialization add event listeners
+            for (var i = 0; i < board.boardPieces.length; i++) {
+                for (var j = 0; j < board.boardPieces[i].length; j++) {
+                    document.getElementById(board.boardPieces[i][j]).addEventListener("click", function() {
+                        board.placePiece(this);
+                        board.pieceCount++;
+        
+                        var output = board.checkForWin();
+        
+                        if (output) {
+                            board.lastWin = output[0];
+                            view.setInner("result", output);
+                        }
+                    })
+                }
             }
-        }
+        
+            //0 out all of the pieces and reset turns
+            document.getElementById("reset").addEventListener("click", function() {
+                for (var i = 0; i < board.boardPieces.length; i++) {
+                    for (var j = 0; j < board.boardPieces[i].length; j++) {
+                        document.getElementById(board.boardPieces[i][j]).innerHTML = "";
+                    }
+                }
+        
+                //REINITIALIZE BOARD
+                //check who won last
+                if (board.lastWin === "X") {
+                    board.playerXTurn = true;
+                } else {
+                    board.playerXTurn = false;
+                }
+                board.pieceCount = 0;
+                view.setInner("result", "");
+            })
+        });
+    }
+}
 
-        //REINITIALIZE BOARD
-        //check who won last
-        if (lastWin === "X") {
-            playerXTurn = true;
-        } else {
-            playerXTurn = false;
-        }
-        pieceCount = 0;
-        document.getElementById("result").innerHTML = "";
-    })
-});
+view.init();
