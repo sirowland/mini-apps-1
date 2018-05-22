bcc9ddd4d47a97103e630c9c3de20fc1ac789876
Sam Rowland <srowland2594@gmail.com>
2018-05-22 11:30:00 -0700

    setup express server, hosting correct files, on submit sends a json body to server

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,15 @@
+$(document).ready(function(){
+    $('#submit').on('click', function(event){
+        event.preventDefault();
 
+        var data = $('#jsonSubmit')[0].value;
+
+        $.ajax({
+            method: 'POST',
+            url: 'http://127.0.0.1:1337/',
+            data: data,
+            contentType: 'application/json'
+        })
+    })
+});
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
 
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '/client')));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+//ATTEMPT AT USING JQUERY IN NODEMODULES
+// app.get('/node_modules/jquery/dist/jquery.min.js', function(req, res){
+//     res.sendFile(path.join(__dirname, 'node_modules/jquery/dist/jquery.min.js'));
+// });
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+
+    
+});
+
+app.listen(1337, () => console.log('Listening on port 1337...'));
 
