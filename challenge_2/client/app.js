968a74964070e5714cab5b8886a160918f64f2a6
Sam Rowland <srowland2594@gmail.com>
2018-05-22 17:26:10 -0700

    Refactored to fetch

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -4,19 +4,34 @@ $(document).ready(function(){
 
         var data = $('#jsonSubmit')[0].value;
 
-        $.ajax({
+        fetch('http://127.0.0.1:1337/',{
             method: 'POST',
-            url: 'http://127.0.0.1:1337/',
-            data: data,
-            contentType: 'application/json',
-            success: function(data) {
-                $('#response').html(data); 
-                $('#jsonSubmit').val('');
-            },
-            error: function(data) {
-
+            body: data,
+            headers: {
+                'content-type': 'application/json'
             }
-        })
+        }).then((res) => {
+            return res.text();
+        }).then((res) => {
+            $('#response').html(res); 
+            $('#jsonSubmit').val('');
+        }).catch((err) => {
+            return console.log(err);
+        });
+
+        // $.ajax({
+        //     method: 'POST',
+        //     url: 'http://127.0.0.1:1337/',
+        //     data: data,
+        //     contentType: 'application/json',
+        //     success: function(data) {
+        //         $('#response').html(data); 
+        //         $('#jsonSubmit').val('');
+        //     },
+        //     error: function(data) {
+
+        //     }
+        // })
     })
 });
 
