95405c894c8639164f420c75fe201354da1cbce1
Sam Rowland <srowland2594@gmail.com>
2018-05-24 11:47:41 -0700

    Setup the app with an express server and React

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
+
+app.use(express.static('./client/dist'));
+app.listen(3000, () => {
+    console.log('Listening on port 3000...');
+});
 
